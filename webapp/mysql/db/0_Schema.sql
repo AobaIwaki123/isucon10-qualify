b9f73c8f8876d8a50300ce7caa990b5090962a5f
AobaIwaki123 <aiwaki@gmail.com>
2024-11-10 22:40:33 +0900

    change schema

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -17,8 +17,11 @@ CREATE TABLE isuumo.estate
     door_height INTEGER             NOT NULL,
     door_width  INTEGER             NOT NULL,
     features    VARCHAR(64)         NOT NULL,
-    popularity  INTEGER             NOT NULL
+    popularity  INTEGER             NOT NULL,
+    popularity_desc INTEGER GENERATED ALWAYS AS (-popularity) STORED NOT NULL
 );
+
+CREATE INDEX estate_popularity_id_idx ON isuumo.estate (popularity_desc, id);
 
 CREATE TABLE isuumo.chair
 (
@@ -34,5 +37,8 @@ CREATE TABLE isuumo.chair
     features    VARCHAR(64)     NOT NULL,
     kind        VARCHAR(64)     NOT NULL,
     popularity  INTEGER         NOT NULL,
-    stock       INTEGER         NOT NULL
+    stock       INTEGER         NOT NULL,
+    popularity_desc INTEGER GENERATED ALWAYS AS (-popularity) STORED NOT NULL
 );
+
+CREATE INDEX chair_popularity_id_idx ON isuumo.chair (popularity_desc, id);
